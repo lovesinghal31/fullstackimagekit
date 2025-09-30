0f2f0e8a9a728a2325686433eac605396a884870
Love Singhal <groothoon31@gmail.com>
2025-09-30 23:50:05 +0530

    Add user and video models with mongoose schemas; update package dependencies

---
+++ src/models/Video.ts
@@ -0,0 +1,47 @@
+import { IVideo, VIDEO_DIMENSIONS } from "@/types/Video";
+import mongoose, { Schema, model, models } from "mongoose";
+
+const videoSchema = new Schema<IVideo>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+    controls: {
+      type: Boolean,
+      default: true,
+    },
+    transformation: {
+      height: {
+        type: Number,
+        default: VIDEO_DIMENSIONS.height,
+      },
+      width: {
+        type: Number,
+        default: VIDEO_DIMENSIONS.width,
+      },
+      quality: {
+        type: Number,
+        min: 1,
+        max: 100,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema);
+
+export default Video;
